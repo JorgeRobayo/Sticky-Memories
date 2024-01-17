aa904a04151a7b4912ffb53947a534348e4d1a86
jorge robayo <jorgerobmor@gmail.com>
2024-01-17 11:57:30 -0500

    set up controlles, models, utilities. code not working websocket error

---
+++ controllers/api/users.js
@@ -0,0 +1,33 @@
+const User = require("../../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt')
+module.exports = {
+  create
+};
+
+
+function createJWT(user) {
+    console.log("Creating JWT")
+    return jwt.sign(
+      // data payload
+      { user },
+      process.env.SECRET,
+      { expiresIn: "24h" }
+    );
+  }
+
+async function create(req, res) {
+    try {
+        // Add the user to the database
+        const user = await User.create(req.body);
+        // token will be a string
+        const token = createJWT(user);
+        // Yes, we can use res.json to send back just a string
+        // The client code needs to take this into consideration
+        res.json(token);
+      } catch (err) {
+        // Client will check for non-2xx status code
+        // 400 = Bad Request
+        res.status(400).json(err);
+      }
+}
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 require('./config/database')
+const express = require("express");
+const app = express();
 const logger = require('morgan')
 
 //setting mongoose
@@ -9,3 +11,6 @@ const db = mongoose.connection
 db.on('connected', ()=>{
     console.log(`connected to ${db.name} at ${db.host}:${db.port}`)
 })
+
+//MIDDLEWARE
+app.use('/api/users', require('./routes/api/users'));
